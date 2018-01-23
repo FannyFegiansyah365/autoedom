dabed8a50799d69741b5935ba2b5a56de9e0ba0e
medreza <31310263+medreza@users.noreply.github.com>
2018-01-23 12:56:47 +0700

    fix 'random baik' range

--- random_baik.js
+++ random_baik.js
@@ -5,7 +5,9 @@ for (m=0;m<jmlPertanyaan;m++)
 {
 	for (n=0;n<3;n++)
 	{
-	var selectorwkwk = '#edom_' + m + '_'+(Math.floor(Math.random() * 3) + 2)+'_' + n;
+	var xx = (Math.floor(Math.random() * 3) + 1);
+	if (xx==1){xx=2};
+	var selectorwkwk = '#edom_' + m + '_'+ xx+'_' + n;
 	console.log(selectorwkwk);
 	jQuery(selectorwkwk).attr('checked', true);
 	}
