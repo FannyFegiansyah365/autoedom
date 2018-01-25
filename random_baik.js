2a4b860447892261a40fd707fc342e454670ef70
medreza <31310263+medreza@users.noreply.github.com>
2018-01-25 09:45:09 +0700

    Improve scoring
    Improvement: More reasonable scoring on 'random_baik'
    Fix: Add new line at the end of file; more meaningful variable name.

--- random_baik.js
+++ random_baik.js
@@ -1,14 +1,26 @@
 // Randomly fills "Baik" or "Sangat Baik" di dua belas pertanyaan EDOM dengan dua dosen pengampu
+// score : 0 -> Buruk
+// score : 1 -> Cukup
+// score : 2 -> Baik
+// score : 3 -> Sangat Baik
 
 var jmlPertanyaan = 12;
+var first_score = 0;
+var prev_score = 0;
 for (m=0;m<jmlPertanyaan;m++)
 {
 	for (n=0;n<3;n++)
 	{
-	var xx = (Math.floor(Math.random() * 3) + 1);
-	if (xx==1){xx=2};
-	var selectorwkwk = '#edom_' + m + '_'+ xx+'_' + n;
-	console.log(selectorwkwk);
-	jQuery(selectorwkwk).attr('checked', true);
+		var score = (Math.floor(Math.random() * 3) + 1);
+		if (score==1){score=2};
+		if (first_score==3 &&  n==2 && prev_score==2){score=3};
+		if (first_score==2 &&  n==2 && prev_score==3){score=2};
+		var selectorwkwk = '#edom_' + m + '_' + score + '_' + n;
+		if (n==0){first_score = score};
+		prev_score = score;
+		console.log(selectorwkwk);
+		jQuery(selectorwkwk).attr('checked', true);
 	}
+	first_score = 0;
+	prev_score = 0;
 }
--- sangatbaik.js
+++ sangatbaik.js
@@ -1,12 +1,17 @@
 // Otomatis ngisi "SANGAT BAIK" di dua belas pertanyaan EDOM dengan dua dosen pengampu
+// score : 0 -> Buruk
+// score : 1 -> Cukup
+// score : 2 -> Baik
+// score : 3 -> Sangat Baik
 
 var jmlPertanyaan = 12;
+var score = 3;
 for (m=0;m<jmlPertanyaan;m++)
 {
 	for (n=0;n<3;n++)
 	{
-	var selectorwkwk = '#edom_' + m + '_3_' + n;
-	console.log(selectorwkwk);
-	jQuery(selectorwkwk).attr('checked', true);
+		var selectorwkwk = '#edom_' + m + '_' + score + '_' + n;
+		console.log(selectorwkwk);
+		jQuery(selectorwkwk).attr('checked', true);
 	}
 }
